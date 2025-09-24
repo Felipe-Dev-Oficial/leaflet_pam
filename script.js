1327c452c71eb4fe3f1d6fcfe8fb4c11420bd2a1
felipe-dev-oficial <duartefelipe020@gmail.com>
2025-09-24 20:34:29 -0300

    teste 1

---
+++ script.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 35,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+function findMe(){
+    if (!navigator.geolocation){
+        alert("Dá um update aí");
+        return;
+    }
+};
+document.getElementById("btn1")
+
+// navigator.geolocation.
